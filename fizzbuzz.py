3b8f461e9a7b374636de5ff72cfceb1f95fb801f
Cássio Botaro <cassiobotaro@gmail.com>
2021-11-06 15:38:39 -0300

    Tradução do arquivo fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,26 @@
-# smalltalk infected fizzbuzz version
+# fizzbuzz em uma versão infectada por smalltalk
+"""
+Notas:
+- Funções são utilizadas para simular blocos de código presentes em smalltalk
+"""
 from collections import deque
 
 from forbiddenfruit import curse
 
 
 def if_true(self, block):
-    # simulate blocks using functions
+    # somente executa a função caso self 
+    # seja algo avaliado como verdadeiro (truthy)
+    # isto é chamado de curto circuito
     self and block()
-    # close circuit when object is truthy
     return self
 
 
 def if_false(self, block):
-    # simulate blocks using functions
+    # somente executa a função caso self 
+    # seja algo avaliado como falso (falsy)
+    # isto é chamado de curto circuito
     not self and block()
-    # close circuit when object is falsy
     return self
 
 
@@ -37,29 +43,30 @@ def do(self, block):
     deque(map(block, self), maxlen=0)
     return self
 
-
+# curse é uma maneira de modificar tipos builtins da linguagem
 curse(bool, "if_true", if_true)
 curse(bool, "if_false", if_false)
 curse(str, "print", println)
 curse(int, "print", println)
 curse(range, "do", do)
 
-# lambdas are used to simulate blocks
-"""Summary
-We add a do methd on range objects that evaluates a block
-for each element on interval.
-This block will receive a number, that evaluated
-in the expression "number % 15 == 0", This will result in a boolean object,
-to which we will send two messages,
-one with a block to be evaluated if the expression is true and
-another for if it is false.
-If true, we will send a print message to a "FizzBuzz" object.
-If it is false, we will use the same numeric object
-to evaluate the expression number% 5 == 0.
-And so we repeat the cycle, until at last a message
-is sent to the number printed.
 """
-range(1, 101).do(
+Explicação:
+Nós adicionamos um método "do" em objetos do tipo "range" que avalia um bloco
+para cada elemento no intervalo. 
+Este bloco faz a avaliação de um número verificando se o mesmo 
+é divisível por 15, e isto resulta em um objeto booleano.
+Para este objeto booleano, duas mensagens são enviadas, uma que diz que caso sua avaliação
+seja verdadeira, o valor "FizzBuzz" deve ser exibido em tela. A outra informa que se o objeto é 
+avaliado como falso, um novo objeto booleano deve ser retornado. Este novo objeto
+booleano recebe duas mensagens, uma informando ao objeto que caso sua avaliação seja verdadeira,
+a mensagem "Buzz" deve ser exibida na tela e outra que caso sua avaliação seja falsa um 
+novo objeto booleano deve ser retornado.
+Por fim este novo objeto booleano também recebe duas mensagens, uma informando que se sua avaliação for verdadeira
+"Fizz" deve ser exibido em tela e outra que caso seja avaliado como falso, o número deve ser exibido na tela.
+"""
+range(1, 101).do( 
+    # lambdas também são utilizadas para simular blocos 
     lambda number: (number % 15 == 0)
     .if_true("FizzBuzz".print)
     .if_false(
@@ -72,11 +79,4 @@ range(1, 101).do(
         )
     )
 )
-
-"""
-Notes:
-- A message is sent to an object for printing
-- Lambdas are used to simulate a block
-- Add method do for a range, evaluating a block on each number on interval
-- Objects and messages
-"""
+# Nota: É importante notar que no fim o que temos são objetos trocando mensagens entre si.
